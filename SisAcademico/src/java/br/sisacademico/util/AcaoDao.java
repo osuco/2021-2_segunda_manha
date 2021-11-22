873d4854d1cf7c44d69006f00dd2a517285aaebf
Prof. Thiago G. Traue <t.traue@gmail.com>
2021-11-22 11:25:51 -0300

    Continuação do projeto

---
+++ SisAcademico/src/java/br/sisacademico/util/AcaoDao.java
@@ -0,0 +1,8 @@
+package br.sisacademico.util;
+
+public enum AcaoDao {
+    leitura,
+    cadastro,
+    edicao,
+    exclusao;
+}
---
+++ SisAcademico/src/java/br/sisacedemico/dao/CursoDAO.java
@@ -0,0 +1,48 @@
+package br.sisacedemico.dao;
+
+import br.sisacademico.model.Curso;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+//Essa classe é responsável por fazer o CRUD de Cursos
+public class CursoDAO {
+
+    private static Statement stm = null;
+    
+    public Map<Curso, Integer> getTodosCursosCountAlunos() throws SQLException {
+        
+        String query = "SELECT ID_CURSO, NOME_CURSO, TIPO_CURSO,"
+                + "    (SELECT COUNT(*) "
+                + "       FROM TB_ALUNO"
+                + "      WHERE ID_CURSO = CURSO.ID_CURSO) AS QTD_ALUNOS"
+                + " FROM"
+                + "    TB_CURSO as CURSO"
+                + " ORDER BY"
+                + "    CURSO.NOME_CURSO";
+        
+        Map<Curso, Integer> relatorio = new HashMap<>();
+        
+        stm = ConnectionFactory.getConnection().createStatement(
+                ResultSet.TYPE_SCROLL_INSENSITIVE,
+                ResultSet.CONCUR_READ_ONLY);
+        
+        ResultSet resultados = stm.executeQuery(query);
+        
+        while(resultados.next()) {
+            Curso c = new Curso();
+            c.setIdCurso(resultados.getInt("ID_CURSO"));
+            c.setNomeCurso(resultados.getString("NOME_CURSO"));
+            c.setTipoCurso(resultados.getString("TIPO_CURSO"));
+            int qtdAlunos = resultados.getInt("QTD_ALUNOS");
+            
+            relatorio.put(c, qtdAlunos);
+        }
+        
+        stm.getConnection().close();
+        
+        return relatorio;
+    }
+}
